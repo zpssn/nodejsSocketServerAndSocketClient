7085a1d825292a1889311d23b559be9afebee177
zpssn <920167280@qq.com>
2016-11-29 09:29:11 +0800

    Update index.js
    添加进程守护

--- routes/index.js
+++ routes/index.js
@@ -77,4 +77,11 @@ router.get('/', function(req, res, next) {
         }
       }
 });
+process.on('uncaughtException', function (err) {
+  //打印出错误
+  console.log(err);
+  //打印出错误的调用栈方便调试
+  console.log(err.stack)；
+});
+
 module.exports = router;
